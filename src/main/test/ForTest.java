5553a2118b3ad567afd8dfe27af400a9889ce308
1140941668@qq.com <wang710943384>
2020-09-15 17:16:39 +0800

    if else test

---
+++ src/main/test/ForTest.java
@@ -0,0 +1,15 @@
+package main.test;
+
+public class ForTest {
+    public static void main(String[] args) {
+        for (int x = 1;x <= 9;x++){
+            for (int y = 1;y <= x;y++){
+                System.out.print(y + "*" + x + "=" + x * y + " ");
+                if (x * y < 10){
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
